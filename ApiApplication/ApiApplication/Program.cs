b9a052cb94c01949a64bd0c53b944b2ad4137d56
Jaffar Abbas <gamaportal8@gmail.com>
2023-03-06 19:25:44 +0500

    3-6-2023

---
+++ ApiApplication/ApiApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using ApiApplication.Model;
+using System.Threading.Tasks;
+using System;
+
+namespace ApiApplication
+{
+    public class Program
+    {
+        private const string apiUrl = "https://jsonplaceholder.typicode.com";
+        private const string endPoint = "users";
+
+        public async void runner()
+        {
+            var apiHandler = new ApiHandler<User>(apiUrl, endPoint);
+            Console.WriteLine("Get All Users");
+            var result = await apiHandler.GetAllAsync();
+            foreach (var data in result)
+            {
+                Console.WriteLine($"ID: {data.Id}, Name: {data.Name}");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.runner();
+        }
+    }
+}
---
+++ ApiApplication/ApiHandler/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class ApiHandler<T>
+{
+    private readonly string _baseUrl;
+    private readonly string _resource;
+
+    public ApiHandler(string baseUrl, string resource)
+    {
+        _baseUrl = baseUrl;
+        _resource = resource;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        var url = $"{_baseUrl}/{_resource}/{id}";
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var entity = JsonConvert.DeserializeObject<T>(responseContent);
+            return entity;
+        }
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        var url = $"{_baseUrl}/{_resource}";
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var entities = JsonConvert.DeserializeObject<List<T>>(responseContent);
+            return entities;
+        }
+    }
+
+    public async Task<int> CreateAsync(T entity)
+    {
+        var url = $"{_baseUrl}/{_resource}";
+        using (var client = new HttpClient())
+        {
+            var json = JsonConvert.SerializeObject(entity);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var id = JsonConvert.DeserializeObject<int>(responseContent);
+            return id;
+        }
+    }
+
+    public async Task UpdateAsync(int id, T entity)
+    {
+        var url = $"{_baseUrl}/{_resource}/{id}";
+        using (var client = new HttpClient())
+        {
+            var json = JsonConvert.SerializeObject(entity);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync(url, content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var url = $"{_baseUrl}/{_resource}/{id}";
+        using (var client = new HttpClient())
+        {
+            var response = await client.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
